ade42f1d1043b79952b04fd9f0856ecc575417da
maciejbandyk <maciejbandyk@gmail.com>
2020-02-26 14:59:33 +0100

    Add Character DTO

---
+++ Models/CharacterDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StarWarsService.Models
+{
+    public class CharacterDTO
+    {
+        public int CharacterId { get; set; }
+        public string Name { get; set; }
+        public string[] CharacterEpisodes { get; set; }
+        public string Planet { get; set; }
+        public string[] CharacterFriends { get; set; }
+    }
+}
